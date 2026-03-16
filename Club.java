0dfef76a0514b8983ddd2909535150b8c5526aa7
Anuj Patel <patelanuj452@gmail.com>
2026-03-16 10:46:03 -0400

    a9-10

--- Club.java
+++ Club.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 /**
  * Store details of club memberships.
  * 
@@ -36,4 +37,52 @@ public class Club
     {
         return members.size(); //return collection.size
     }
+    
+    public int joinedInMonth(int month)
+    {
+    if (month < 1 || month > 12) {
+        System.out.println("Error: month must be between 1 and 12.");
+        return 0;
+    }
+
+    int count = 0;
+    for (Membership m : members) {
+        if (m.getMonth() == month) {
+            count++;
+        }
+    }
+    return count;
+    }
+
+    
+    
+    /**
+    * Remove from the club's collection all members who
+    * joined in the given month, and return them stored
+    * in a separate collection object.
+    * @param month The month of the membership.
+    * @param year The year of the membership.
+    * @return The members who joined in the given month and year.
+    */
+   public ArrayList<Membership> purge(int month, int year)
+    {
+    ArrayList<Membership> removed = new ArrayList<>();
+
+    if (month < 1 || month > 12) {
+        System.out.println("Error: month must be between 1 and 12.");
+        return removed;  // empty list
+    }
+
+    Iterator<Membership> it = members.iterator();
+    while (it.hasNext()) {
+        Membership m = it.next();
+        if (m.getMonth() == month && m.getYear() == year) {
+            it.remove();       // remove from club
+            removed.add(m);    // add to return list
+        }
+    }
+
+    return removed;
+    }
+
 }
